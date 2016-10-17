5ac95aea9131be81d9d2100bd52fe0eda8af73c1
CIMSOLUTIONS\k.lalaoui <k.lalaoui@cimsolutions.nl>
2016-10-17 17:00:26 +0200

    front-end bestelling verdelen

---
+++ front_end/app/OrderList/order-list.component.ts
@@ -0,0 +1,15 @@
+// Imports
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+
+@Component({
+  moduleId: module.id,
+  selector: 'OrderList',
+  templateUrl : `order-list.component.html`
+})
+
+// Component class implementing OnInit
+export class OrderListComponent{
+}
