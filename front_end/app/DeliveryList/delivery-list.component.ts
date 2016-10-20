0baa62d2aa0081878f5ef75aa62b6639a6d4860e
CIMSOLUTIONS\Z.Huraibi <Z.Huraibi@cimsolutions.nl>
2016-10-20 16:45:29 +0200

    Added get order in front end

--- front_end/app/DeliveryList/delivery-list.component.ts
+++ front_end/app/DeliveryList/delivery-list.component.ts
@@ -5,29 +5,28 @@ import { Http } from '@angular/http';
 import 'rxjs/add/operator/toPromise';
 import {Order} from './Order';
 import { ActivatedRoute, Params }   from '@angular/router';
-import { TruckService } from './truck.service';
+import { OrderService } from './order.service';
 
 @Component({
   moduleId: module.id,
   selector: 'DeliveryList',
   templateUrl: `delivery-list.component.html`,
-  providers: [TruckService]
+  providers: [OrderService]
 })
 
 // Component class implementing OnInit
 export class DeliveryListComponent implements OnInit {
-  _truckService: TruckService;
   order: Order;
-  
-   orderId: number;
-   orderURL: string;
 
-  constructor(private http: Http, private route: ActivatedRoute) {
+  orderId: number;
+  orderURL: string;
+
+  constructor(private http: Http, private route: ActivatedRoute, private _orderService: OrderService) {
   }
 
   printDeliveryList(divName) {
     var printContents = document.getElementById(divName).innerHTML;
-  
+
     var originalContents = document.body.innerHTML;
 
     document.body.innerHTML = printContents;
@@ -35,43 +34,38 @@ export class DeliveryListComponent implements OnInit {
     window.print();
 
     document.body.innerHTML = originalContents;
-    
+
     window.clearInterval(2);
 
   }
-    
 
-    
+
+
   ngOnInit() {
     this.route.params.subscribe(
       (param: any) => {
         let id = param['id'];
-//        console.log(id);
+        //        console.log(id);
         this.orderId = id;
       });
-    
-    this.orderURL = "http://localhost:8080/snel-transport/api/orders/" + this.orderId.toString();
-    
-    console.log(this.orderId);
-    
-    this.http.get(this.orderURL).
-      toPromise().then(r => r.json()).then(r => this.order = r);
-    console.log("qwe res");
-    console.log(this.order);
-    
-//    TO DO:
-    this._truckService.getTrucks()
+
+
+
+    this._orderService.getOrder(this.orderId)
       .subscribe(
       res => {
-        console.log("app trucks ");
-        //log the response which shows a session
-        console.log("truck yeye");
-        //                  this.truck = res;
+        console.log("app res ");
+        console.log(res);
+        this.order = res;
+        console.log("order res ");
+        console.log(this.order);
       },
       error => {
         console.log("app trucks error");
         console.log(error);
       }
       );
+      console.log(" outside res order ");
+      console.log(this.order);
   }
 }
