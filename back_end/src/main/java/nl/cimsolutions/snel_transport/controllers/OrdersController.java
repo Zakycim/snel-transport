2d846e8aeab8c0ec6199d16c4fbf43e99e282b2f
Sebastiaan Mekes <S.mekes@CIMSOLUTIONS.local>
2016-10-28 17:37:38 +0200

    Orders verdelen over trucks werkt met datum

--- back_end/src/main/java/nl/cimsolutions/snel_transport/controllers/OrdersController.java
+++ back_end/src/main/java/nl/cimsolutions/snel_transport/controllers/OrdersController.java
@@ -40,197 +40,214 @@ import nl.cimsolutions.snel_transport.services.OrderListFacade;
 @Path("orders")
 public class OrdersController {
 
-    /**
-     * Method handling HTTP GET requests. The returned object will be sent to
-     * the client as "text/plain" media type.
-     *
-     * @return String that will be returned as a text/plain response.
-     */
+	/**
+	 * Method handling HTTP GET requests. The returned object will be sent to
+	 * the client as "text/plain" media type.
+	 *
+	 * @return String that will be returned as a text/plain response.
+	 */
 
-    OrdersFacade orderFacade = new OrdersFacade();
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<Orders> getAllOrders() {
-        System.out.println("getallorders");
-//        OrdersFacade orderFacade = new OrdersFacade();
-        List<Orders> orders = orderFacade.findAll();// findAll();
+	OrdersFacade orderFacade = new OrdersFacade();
 
-        System.out.println(orders);
-        System.out.println("getallorders end");
-        return orders;
-    }
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Orders> getAllOrders() {
+		System.out.println("getallorders");
+		// OrdersFacade orderFacade = new OrdersFacade();
+		List<Orders> orders = orderFacade.findAll();// findAll();
 
-    @GET
-    @Path("/{id}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getOrderById(@PathParam("id") long id) {
-        OrdersFacade orderFacade = new OrdersFacade();
+		System.out.println(orders);
+		System.out.println("getallorders end");
+		return orders;
+	}
 
-        Orders order = orderFacade.find(id);
-        
-        // Check if order is empty
-        if(order == null){
-        	return Response.status(Response.Status.BAD_REQUEST).entity("Couldn't find customer").build();
-        } else {
-        	return Response.status(Response.Status.OK).entity(order).build();
-        }
+	@GET
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getOrderById(@PathParam("id") long id) {
+		OrdersFacade orderFacade = new OrdersFacade();
 
-    }
-    
-    @GET
-    @Path("/deliverylist")
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<OrderList> getOrderList() {
-    	System.out.println("getallorders");
-    	OrderListFacade orderListFacade = new OrderListFacade();
-	      List<OrderList> orderlists = orderListFacade.findAll();// findAll();
+		Orders order = orderFacade.find(id);
 
-	      return orderlists;
-    }
+		// Check if order is empty
+		if (order == null) {
+			return Response.status(Response.Status.BAD_REQUEST).entity("Couldn't find customer").build();
+		} else {
+			return Response.status(Response.Status.OK).entity(order).build();
+		}
 
-    @POST
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response addOrder(Orders data) {
-        if(data.getCustomer().getId() == null) {
-            return Response.status(Response.Status.BAD_REQUEST).entity("customer ID is required").build();
-        }
-        
-        CustomerFacade customerFacade = new CustomerFacade();
-        Customer customer = customerFacade.find(data.getCustomer().getId());
-        
-        if(customer == null) {
-            return Response.status(Response.Status.BAD_REQUEST).entity("customerID wasn't found").build();
-        }
-        
-        Orders order = new Orders();
-        StatusFacade statusFacade = new StatusFacade();
+	}
 
-        Status status = statusFacade.find(1L);
-        order.setCustomer(customer);
+	@GET
+	@Path("/deliverylist")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<OrderList> getOrderList() {
+		System.out.println("getallorders");
+		OrderListFacade orderListFacade = new OrderListFacade();
+		List<OrderList> orderlists = orderListFacade.findAll();// findAll();
 
-        Date orderDate = new Date();
-        order.setOrderDate(orderDate);
-        order.setStatus(status);
-        
-        if (data.getOrderLines() == null) {
-            return Response.status(Response.Status.BAD_REQUEST).entity("Order lines are required").build();
-        }
-        
-        List<OrderLine> orderLines = new ArrayList<OrderLine>(); 
-        Product product = new Product();
-        ProductFacade productFacade = new ProductFacade();
-        
-        for (int i = 0; i < data.getOrderLines().size(); i++) {
-            OrderLine orderLine = new OrderLine();
-            product = productFacade.find(data.getOrderLines().get(i).getProduct().getId());
-            if(product == null) {
-                return Response.status(Response.Status.BAD_REQUEST).entity("product ID wasn't found").build();
-            }
-            
-            orderLine.setProduct(product);
-            orderLine.setAmount(data.getOrderLines().get(i).getAmount());
-            orderLines.add(orderLine);
-        }
-        
-        order.setOrderLines(orderLines);
-        
+		return orderlists;
+	}
 
-        OrdersFacade orderFacade = new OrdersFacade();
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response addOrder(Orders data) {
+		if (data.getCustomer().getId() == null) {
+			return Response.status(Response.Status.BAD_REQUEST).entity("customer ID is required").build();
+		}
 
-        Orders newlyOrder = new Orders();
-        newlyOrder = orderFacade.create(order);
+		CustomerFacade customerFacade = new CustomerFacade();
+		Customer customer = customerFacade.find(data.getCustomer().getId());
 
-        return Response.status(Response.Status.CREATED).entity(newlyOrder).build();
-    }
+		if (customer == null) {
+			return Response.status(Response.Status.BAD_REQUEST).entity("customerID wasn't found").build();
+		}
 
-    @POST
-    @Path("/orderlines")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response addOrderLine(OrderLine[] data) {
-        OrderLineFacade orderLineFacade = new OrderLineFacade();
-        // Print all the array elements
-        for (int i = 0; i < data.length; i++) {
-            OrderLine orderLine = new OrderLine();
-            orderLine.setOrderId(data[i].getOrderId());
-            orderLine.setProduct(data[i].getProduct());
-            orderLine.setAmount(data[i].getAmount());
-            orderLineFacade.create(orderLine);
-        }
+		Orders order = new Orders();
+		StatusFacade statusFacade = new StatusFacade();
 
-        return Response.status(Response.Status.CREATED).entity(data).build();
+		Status status = statusFacade.find(1L);
+		order.setCustomer(customer);
 
-    }
-    
-    @POST
-    @Path("/deliverylist")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response addOrderList(OrderList data) {
-    	OrderListFacade orderListFacade = new OrderListFacade();
-    	TruckFacade truckFacade = new TruckFacade();
-    	OrdersFacade ordersFacade = new OrdersFacade();
-        // Print all the array elements
-//        OrderList orderList = new OrderList();
-//        Orders order = new Orders();
-//        Truck truck = new Truck();
-//        truck.setId(data.getTruck().getId());
-//        truck = truckFacade.find(truck.getId());
-//        orderList.setOrder(order);
-//        orderList.setTruck(data.getTruck());
-//        orderListFacade.create(orderList);
-    	
-    	// calculatie van pallavi aantal orders voor 10 uur
-        int pallavidatumshizzle = 5;
-        
-        for (int i = 0; i < pallavidatumshizzle; i++) {
-        	Orders order = new Orders();
-        	OrderList orderList = new OrderList();
-        	order.setId((long)i+1);
-        	orderList.setOrder(order);
-            orderList.setTruck(data.getTruck());
-            orderListFacade.create(orderList);
-        }
-        
-        // Lezen met welk nummer de eerste orderlist begint
-        
-        // roep method op van khaoula en meesturen in parameter
-//        verdeelOrders(startInt)
+		Date orderDate = new Date();
+		order.setOrderDate(orderDate);
+		order.setStatus(status);
 
+		if (data.getOrderLines() == null) {
+			return Response.status(Response.Status.BAD_REQUEST).entity("Order lines are required").build();
+		}
 
-        return Response.status(Response.Status.CREATED).entity("blabla").build();
-    }
-    
-    @PUT
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response editOrder(Orders data) {
-    	Orders order = new Orders();
-    	Orders editedOrder = new Orders();
-    	System.out.println("editOrder");
-    	System.out.println("data id " + data.getId());
-    	
-        order = orderFacade.find(data.getId());
-        order.setStatus(data.getStatus());
-        
-//        order = orderFacade.edit(order);
-//    	return Response.status(Response.Status.CREATED).entity(order).build();
-        try {
-            editedOrder = orderFacade.edit(order);
-        	return Response.status(Response.Status.CREATED).entity(editedOrder).build();
+		List<OrderLine> orderLines = new ArrayList<OrderLine>();
+		Product product = new Product();
+		ProductFacade productFacade = new ProductFacade();
+
+		for (int i = 0; i < data.getOrderLines().size(); i++) {
+			OrderLine orderLine = new OrderLine();
+			product = productFacade.find(data.getOrderLines().get(i).getProduct().getId());
+			if (product == null) {
+				return Response.status(Response.Status.BAD_REQUEST).entity("product ID wasn't found").build();
+			}
+
+			orderLine.setProduct(product);
+			orderLine.setAmount(data.getOrderLines().get(i).getAmount());
+			orderLines.add(orderLine);
+		}
+
+		order.setOrderLines(orderLines);
+
+		OrdersFacade orderFacade = new OrdersFacade();
+
+		Orders newlyOrder = new Orders();
+		newlyOrder = orderFacade.create(order);
+
+		return Response.status(Response.Status.CREATED).entity(newlyOrder).build();
+	}
+
+	@POST
+	@Path("/orderlines")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response addOrderLine(OrderLine[] data) {
+		OrderLineFacade orderLineFacade = new OrderLineFacade();
+		// Print all the array elements
+		for (int i = 0; i < data.length; i++) {
+			OrderLine orderLine = new OrderLine();
+			orderLine.setOrderId(data[i].getOrderId());
+			orderLine.setProduct(data[i].getProduct());
+			orderLine.setAmount(data[i].getAmount());
+			orderLineFacade.create(orderLine);
+		}
+
+		return Response.status(Response.Status.CREATED).entity(data).build();
+
+	}
+
+	@POST
+	@Path("/deliverylist")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response addOrderList() {
+		OrderListFacade orderListFacade = new OrderListFacade();
+		TruckFacade truckFacade = new TruckFacade();
+		OrdersFacade ordersFacade = new OrdersFacade();
+		List<OrderList> orderlists = new ArrayList<>();
+		Long lastOrderListID = 1L;
+		OrderList lastOrderList = new OrderList();
+		int availableTime = 480;
+		TruckFacade tf = new TruckFacade();
+		 List<Truck> trucks = tf.getAllTrucks();
+		
+		// Print all the array elements
+
+		// clear the table of orderlist
+		orderListFacade.clearTable();
+		
+		// calculatie van pallavi aantal orders voor 10 uur
+		List<Orders> orders = ordersFacade.getOrdersByDates();
+		int j = 0;
+		int truckSize = trucks.size();
+
+		int pallavidatumshizzle = 5;
+
+		for (int i = 0; i < orders.size(); i++) {
+			
+			OrderList orderList = new OrderList();
+	
+			//assign truck to orderlist
+			if(availableTime <= 0){
+				j++;
+				availableTime = 480;
+			}
+			
+			if (j == truckSize) {
+				break;
+			}
+			else{
+				orderList.setOrder(orders.get(i));
+                orderList.setTruck(trucks.get(j).getId());
+                
+                orderListFacade.create(orderList);
+    			orderlists.add(orderList);
+    			System.out.println("hier :" + orders.get(i));
+			}
+			// bestellingtijd - 60
+			availableTime -= 60;
+		}
+
+		return Response.status(Response.Status.CREATED).entity("").build();
+	}
+
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response editOrder(Orders data) {
+		Orders order = new Orders();
+		Orders editedOrder = new Orders();
+		System.out.println("editOrder");
+		System.out.println("data id " + data.getId());
+
+		order = orderFacade.find(data.getId());
+		order.setStatus(data.getStatus());
+
+		// order = orderFacade.edit(order);
+		// return
+		// Response.status(Response.Status.CREATED).entity(order).build();
+		try {
+			editedOrder = orderFacade.edit(order);
+			return Response.status(Response.Status.CREATED).entity(editedOrder).build();
 		} catch (Exception e) {
 			return Response.status(Response.Status.BAD_REQUEST).entity("Couldn't update order status").build();
 		}
-    }
+	}
 
-    @GET
-    @Path("/{id}/orderlines")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Orders getOrderLines(@PathParam("id") long id) {
-//        OrdersFacade orderFacade = new OrdersFacade();
+	@GET
+	@Path("/{id}/orderlines")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Orders getOrderLines(@PathParam("id") long id) {
+		// OrdersFacade orderFacade = new OrdersFacade();
 
-        Orders order = orderFacade.find(id);
+		Orders order = orderFacade.find(id);
 
-        return order;// Response.status(Response.Status.CREATED).entity(value).build();
+		return order;// Response.status(Response.Status.CREATED).entity(value).build();
 
-    }
+	}
 }
